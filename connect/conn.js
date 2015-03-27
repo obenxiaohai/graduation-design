c1a10a51f6ff412ffee96ec37526453f769b6123
obenxiaohai <805065973@qq.com>
2015-03-27 22:58:46 +0800

    0.0.3

--- connect/conn.js
+++ connect/conn.js
@@ -2,7 +2,18 @@ var net = require('net');
 var server = net.createServer();
 var sockets = [];
 
-moudule.exports = function myServer(){
+exports = function myServer(){
 	return server;
 };
 
+server.on('error',function(err){
+	console.log('Server error:',err.message);
+});
+
+server.on('close',function(){
+	console.log('Server closed.');
+	var index = sockets.indexOf(socket);
+	sockets.splice(index,1);
+});
+
+server.listen(4001);
